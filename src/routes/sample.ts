e078d80325e807cf204b66e7cb564c23759ab70d
BadLuckZ <thanagorn8802@gmail.com>
2025-08-19 13:17:06 +0700

    feat: add swagger

--- src/routes/sample.ts
+++ src/routes/sample.ts
@@ -3,6 +3,15 @@ import { getSample } from "../controllers/sample";
 
 const sampleRouter = express.Router();
 
+/**
+ * @openapi
+ * /sample:
+ *   get:
+ *     summary: Sample GET
+ *     responses:
+ *       200:
+ *         description: Returns a sample GET response
+ */
 sampleRouter.get("/", getSample);
 
 export default sampleRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import express, { Application } from "express";
 import sampleRouter from "./routes/sample";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 
@@ -9,6 +11,22 @@ const app: Application = express();
 app.use(express.json());
 
 app.use("/sample", sampleRouter);
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Project Baan Saat API",
+      version: "1.0.0",
+      description: "API documentation for Project Baan Saat",
+    },
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const port = process.env.PORT || 5000;
 const node_env = process.env.NODE_ENV || "development";
