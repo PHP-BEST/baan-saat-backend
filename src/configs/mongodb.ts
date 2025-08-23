ed44f145c389d2df630d0c803ee63e26e58d2103
BadLuckZ <thanagorn8802@gmail.com>
2025-08-23 19:19:54 +0700

    fix: Remove NODE_ENV

--- src/configs/mongodb.ts
+++ src/configs/mongodb.ts
@@ -1,16 +1,26 @@
 import mongoose from 'mongoose';
+import path from 'path';
 
 const connectDB = async () => {
   mongoose.set('strictQuery', true);
-  let mongoUri: string = '';
-  if (process.env.NODE_ENV === 'production') {
+
+  const entryFile = process.argv[1] || '';
+  const isProd = entryFile.includes(`${path.sep}dist${path.sep}`);
+
+  let mongoUri: string;
+
+  if (isProd) {
+    console.log('🚀 Using PRODUCTION Database 🚀');
     mongoUri = process.env.MONGO_URI_PROD || '';
   } else {
+    console.log('🔧 Using DEVELOPMENT Database 🔧');
     mongoUri = process.env.MONGO_URI_DEV || '';
   }
+
   if (!mongoUri) {
-    throw new Error('MONGO_URI environment variable is not defined');
+    throw new Error('MONGO_URI is not defined');
   }
+
   await mongoose.connect(mongoUri);
 };
 
