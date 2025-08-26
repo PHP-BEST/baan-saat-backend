edb5882bd50946f0854e07c7d2426ee2fb68b299
Thanagorn Chaiyut <thanagorn8802@gmail.com>
2025-08-26 12:08:35 +0700

    fix: add path to cors (#26)

--- src/configs/cors.ts
+++ src/configs/cors.ts
@@ -2,4 +2,5 @@ export const allowedOrigins = [
   'https://baan-saat-frontend.vercel.app',
   'https://baan-saat-frontend-dev.vercel.app',
   'http://localhost:5173',
+  'http://localhost:5000',
 ];
