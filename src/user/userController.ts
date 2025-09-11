19b734d82f1421e86d3b1413912908be1b6cb4c2
Shinatip Pattanapitoon <shinatip.ptn@gmail.com>
2025-09-11 13:08:27 +0700

    change getUserSession to send all fields

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,12 +2,7 @@ import { Request, Response } from 'express';
 
 export const getUserSession = async (req: Request, res: Response) => {
   if (req.user) {
-    const user = req.user as any;
-    res.json({
-      name: user.name,
-      role: user.role,
-      avatarUrl: user.avatarUrl,
-    });
+    res.json(req.user as any);
   } else {
     res.send();
   }
