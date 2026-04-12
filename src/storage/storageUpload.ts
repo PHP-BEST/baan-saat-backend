d2144d59c164b739711c85972e0c79c8e694e0d5
art1722 <artistitsart172@gmail.com>
2026-04-12 20:41:02 +0700

    fix: migrate storage from DigitalOcean Spaces to AWS S3

--- src/storage/storageUpload.ts
+++ src/storage/storageUpload.ts
@@ -6,7 +6,6 @@ import multerS3 from 'multer-s3';
 
 const s3Client = new S3({
   forcePathStyle: false, // Configures to use subdomain/virtual calling format.
-  endpoint: 'https://sgp1.digitaloceanspaces.com',
   region: 'ap-southeast-1',
   credentials: {
     accessKeyId: spaceKey,
